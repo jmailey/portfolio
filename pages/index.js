5b5628d9d160f94f627789d13c2b97f946388926
jmailey <johnmmailey@gmail.com>
2023-10-03 15:15:13 +0100

    remove resume

--- pages/index.js
+++ pages/index.js
@@ -34,17 +34,6 @@ export default function Home() {
                   title={darkMode ? "Light mode" : "Dark mode"}
                 />
               </li>
-              <li>
-                <a
-                  download
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href="/johnmailey_resume.docx"
-                  className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
-                >
-                  Resume
-                </a>
-              </li>
             </ul>
           </nav>
           <div className="p-10 text-center">
@@ -149,7 +138,7 @@ export default function Home() {
           <h3 className="text-3xl py-1 dark:text-white mb-3">Portfolio</h3>
           <div className="grid grid-cols-1 sm:grid-cols-2 gap-10">
             <div className="text-center shadow-lg border p-4 rounded-xl dark:bg-white">
-              <div class="relative  h-[500px] mx-auto">
+              <div class="relative h-[500px] mx-auto">
                 <Image src={recapi} className="object-contain" fill />
               </div>
               <Link
